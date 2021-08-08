6e2c22663eb959363245bb24c211dff199eeca49
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-08 16:34:04 +0900

    ページの雛形作成、ログインページのルーティング

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import theme from "./theme/theme"
 import { Button, ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import theme from "./theme/theme"
+import { Router } from './router/Router';
 
 function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
-        <Button colorScheme="teal">ボタン</Button>
-        <p>テストテキスト</p>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
       </ChakraProvider>
     </div>
   );
