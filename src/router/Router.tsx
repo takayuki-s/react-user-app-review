36d9ac10d3a42a669a91ab29408bc9e00895bfc2
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-09 05:50:00 +0900

    ルーティング設定

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,6 +1,7 @@
 import { VFC, memo } from "react";
 import { Route, Switch } from "react-router-dom";
 import { Login } from "../components/pages/Login";
+import { homeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo (() => {
   return (
@@ -8,6 +9,15 @@ export const Router: VFC = memo (() => {
       <Route exact path="/">
         <Login />
       </Route>
+      <Route path="/home" render={({ match: { url }}) => (
+        <Switch>
+          {homeRoutes.map((route) => (
+            <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
+              {route.children}
+            </Route>
+          ))}
+        </Switch>
+      )} />
     </Switch>
   )
 })
