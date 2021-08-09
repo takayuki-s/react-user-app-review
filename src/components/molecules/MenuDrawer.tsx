44fad65f712c4c365fb3803fc8a88a0f8d2f3d0b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-10 06:22:51 +0900

    画面遷移

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -4,18 +4,21 @@ import { VFC, memo } from "react";
 type Props = {
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
 }
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const { onClose, isOpen, onClickHome, onClickUserManagement, onClickSetting } = props;
   return (
     <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
       <DrawerOverlay>
         <DrawerContent>
           <DrawerBody p={0} bg="gray.100">
-            <Button w="100%">TOP</Button>
-            <Button w="100%">ユーザー一覧</Button>
-            <Button w="100%">設定</Button>
+            <Button onClick={onClickHome} w="100%">TOP</Button>
+            <Button onClick={onClickUserManagement} w="100%">ユーザー一覧</Button>
+            <Button onClick={onClickSetting} w="100%">設定</Button>
           </DrawerBody>
         </DrawerContent>
       </DrawerOverlay>
