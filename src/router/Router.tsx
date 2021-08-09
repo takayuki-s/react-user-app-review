3163c5c4bf5ef37db2e0fb1854cf7391ca8ae2b8
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-09 09:00:51 +0900

    ヘッダー作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,7 +1,9 @@
 import { VFC, memo } from "react";
 import { Route, Switch } from "react-router-dom";
+
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { homeRoutes } from "./HomeRoutes";
 
 export const Router: VFC = memo (() => {
@@ -14,7 +16,9 @@ export const Router: VFC = memo (() => {
         <Switch>
           {homeRoutes.map((route) => (
             <Route key={route.path} exact={route.exact} path={`${url}${route.path}`}>
-              {route.children}
+              <HeaderLayout>
+                {route.children}
+              </HeaderLayout>
             </Route>
           ))}
         </Switch>
