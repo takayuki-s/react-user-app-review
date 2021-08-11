82f6ef6537dff2b6b038af4d9b7ea412c277bc13
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-12 08:46:52 +0900

    メッセージ表示

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,20 +2,23 @@ import axios from "axios";
 import { useHistory } from "react-router-dom";
 import { useCallback, useState } from "react"
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
   const [ loading, setLoading ] = useState(false);
 
   const login = useCallback((id: string) => {
     setLoading(true)
     axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`).then((res) => {
       if (res.data) {
+        showMessage({ title: "ログインしました", status:"success"})
         history.push("/home")
       } else {
-        alert("ユーザーが見つかりませんでした")
+        showMessage({ title: "ユーザーが見つかりません", status:"error"})
       }
-    }).catch(() => alert("ログインできません")).finally(() => {
+    }).catch(() => showMessage({ title: "ログインできません", status:"error"})).finally(() => {
       setLoading(false)
     })
   }, []);
