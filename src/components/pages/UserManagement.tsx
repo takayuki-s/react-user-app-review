7cfa5d02032c6fe1b4492eee94500584de894885
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-12 08:52:39 +0900

    Wrapの基本的な使い方

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,5 +1,14 @@
+import { Wrap, WrapItem } from "@chakra-ui/react";
 import { VFC, memo } from "react";
 
 export const UserManagement: VFC = memo(() => {
-  return <p>ユーザー管理ページです</p>;
+  return (
+    <Wrap spacing="30px">
+      {[...Array(10)].map(() => (
+        <WrapItem>
+          <div style={{ width: "100px", height: "100px", backgroundColor:"teal"}}></div>
+        </WrapItem>
+      ))}
+    </Wrap>
+  );
 });
