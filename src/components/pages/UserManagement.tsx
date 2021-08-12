7bfeb739497161b7e6bb1c9e498e3850f9ebdba5
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-12 09:04:16 +0900

    ユーザーカード雛形作成

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,14 +1,18 @@
-import { Wrap, WrapItem } from "@chakra-ui/react";
+import { Box, Image, Stack, Wrap, WrapItem, Text } from "@chakra-ui/react";
 import { VFC, memo } from "react";
 
 export const UserManagement: VFC = memo(() => {
   return (
-    <Wrap spacing="30px">
-      {[...Array(10)].map(() => (
-        <WrapItem>
-          <div style={{ width: "100px", height: "100px", backgroundColor:"teal"}}></div>
-        </WrapItem>
-      ))}
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{ cursor: "pointer", opacity: "0.8"}}>
+          <Stack textAlign="center">
+            <Image boxSize="160px" borderRadius="full" src="https://source.unsplash.com/random" alt="プロフィール画像" m="auto"/>
+            <Text fontSize="lg" fontWeight="bold">てけ</Text>
+            <Text fontSize="sm" color="gray">Teke</Text>
+          </Stack>
+        </Box>
+      </WrapItem>
     </Wrap>
   );
 });
