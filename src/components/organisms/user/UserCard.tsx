9eb4716a8bef9de63b13b3e021b923f93dd6a938
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-13 08:03:18 +0900

    モーダル表示（テストメッセージ）

--- src/components/organisms/user/UserCard.tsx
+++ src/components/organisms/user/UserCard.tsx
@@ -5,12 +5,13 @@ type Props = {
   imageUrl: string;
   userName: string;
   fullName: string;
+  onClick: () => void;
 };
 
 export const UserCard: VFC<Props> = memo((props) => {
-  const { imageUrl, userName, fullName} = props;
+  const { imageUrl, userName, fullName, onClick} = props;
   return (
-  <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{ cursor: "pointer", opacity: "0.8"}}>
+  <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{ cursor: "pointer", opacity: "0.8"}} onClick={onClick}>
     <Stack textAlign="center">
       <Image boxSize="160px" borderRadius="full" src={imageUrl} alt={userName} m="auto"/>
       <Text fontSize="lg" fontWeight="bold">{userName}</Text>
--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,11 +1,15 @@
-import { Center, Spinner, Wrap, WrapItem } from "@chakra-ui/react";
+import { Center, Modal, ModalContent, ModalOverlay, Spinner, useDisclosure, Wrap, WrapItem } from "@chakra-ui/react";
+import { useCallback } from "react";
 import { VFC, memo, useEffect } from "react";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
+  const {isOpen, onOpen, onClose} = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
   useEffect(() => getUsers(), [])
+
+  const onClickUser = useCallback(() => onOpen(), []);
 
   return (
     <>
@@ -18,12 +22,18 @@ export const UserManagement: VFC = memo(() => {
           {users.map((user) => (
             <WrapItem key={user.id} mx="auto">
               <Center>
-                <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} />
+                <UserCard imageUrl="https://source.unsplash.com/random" userName={user.username} fullName={user.name} onClick={onClickUser}/>
               </Center>
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <p>テスト</p>
+        </ModalContent>
+      </Modal>
     </>
   );
 });
