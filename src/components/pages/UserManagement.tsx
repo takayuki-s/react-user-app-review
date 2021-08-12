0943adba883d38cdecb0f2252c104c6c65d5a399
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-12 09:53:32 +0900

    ユーザーカード雛形をコンポーネント分割

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,17 +1,12 @@
 import { Box, Image, Stack, Wrap, WrapItem, Text } from "@chakra-ui/react";
 import { VFC, memo } from "react";
+import { UserCard } from "../organisms/user/UserCard";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <Wrap p={{ base: 4, md: 10 }}>
       <WrapItem>
-        <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md" p={4} _hover={{ cursor: "pointer", opacity: "0.8"}}>
-          <Stack textAlign="center">
-            <Image boxSize="160px" borderRadius="full" src="https://source.unsplash.com/random" alt="プロフィール画像" m="auto"/>
-            <Text fontSize="lg" fontWeight="bold">てけ</Text>
-            <Text fontSize="sm" color="gray">Teke</Text>
-          </Stack>
-        </Box>
+        <UserCard imageUrl="https://source.unsplash.com/random" userName="てけ" fullName="teke" />
       </WrapItem>
     </Wrap>
   );
