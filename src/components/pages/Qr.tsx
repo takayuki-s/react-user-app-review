9d73d30d86c79b78a9b66bfe926d1de4ef23d866
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-08-16 11:30:22 +0900

    QRコード表示

--- src/components/pages/Qr.tsx
+++ src/components/pages/Qr.tsx
@@ -1,7 +1,25 @@
+import { Center, Flex } from "@chakra-ui/react";
 import { VFC, memo } from "react";
+import { useQRCode } from "react-qrcodes";
 
 export const Qr: VFC = memo(() => {
+  const [inputRef] = useQRCode({
+    text: 'https://qiita.com/hujuu/items/b12ff32f189f5ab620ca',
+    options: {
+      level: 'H', 
+      margin: 5, 
+      scale: 1, 
+      width: 300,
+    },
+  });
   return (
-    <p>QRコード表示ページのテストです</p>
+    <>
+      <Center>
+        <p>QRコード表示ページのテストです</p>
+      </Center>
+      <Center>
+        <canvas ref={inputRef} />
+      </Center>
+    </>
   )
 });
